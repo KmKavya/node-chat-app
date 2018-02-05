d8edc85de43456beecd382926ab3071e6e922141
kavyavshaiva <kavya.k@evolvussolutions.com>
2018-02-05 18:13:34 +0530

    create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const path=require('path');
 const express=require('express');
 const http=require('http');
 const socketIO=require('socket.io');
+
+const {generateMessage}=require('./utils/message');
 const publicPath=path.join(__dirname,'../public');
-var app=express();
 const port=process.env.PORT || 3000;
+var app=express();
+
 var server=http.createServer(app);
 var io=socketIO(server);
 
@@ -13,24 +16,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=> {
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from:'Admin',
-    text:'Welcome to the chat App',
-    createdAt:new Date().getTime()
-  });
-  socket.broadcast.emit('newMessage', {
-    from:'Admin',
-    text:'New user joined',
-    createdAt:new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat App'));
+  socket.broadcast.emit('newMessage',generateMessage('Admin', 'New user joined'));
 
 socket.on('createMessage' ,(message)=> {
   console.log('createMessage',message);
-  io.emit('newMessage', {
-    from:message.from,
-    text:message.text,
-    createdAt:new Date().getTime()
-  });
+  io.emit('newMessage', generateMessage(message.from,message.text));
 // socket.broadcast.emit('newMessage', {
 //     from:message.from,
 //     text:message.text,
@@ -44,4 +35,4 @@ socket.on('disconnect', ()=> {
 
 server.listen(port, ()=> {
   console.log(`server is up on port ${port}`);
-})
+});
---
+++ server/utils/message.test.js
@@ -0,0 +1,15 @@
+var expect=require('expect');
+
+var {generateMessage}=require('./message');
+
+describe('generateMessage', ()=> {
+  it('should generate correct message object',()=>{
+    var from='kavya';
+    var text='some text';
+    var message=generateMessage(from,text);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from,text});
+  });
+
+});
