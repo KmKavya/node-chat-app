70334518ce371279491eccc0522c9947993cbf23
kavyavshaiva <kavya.k@evolvussolutions.com>
2018-02-05 15:54:23 +0530

    Emit created message on new message

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,6 @@
 var socket=io();
 socket.on('connect',function() {
   console.log(' connected to server');
-
-  socket.emit('createMessage', {
-    from:'kavya',
-    text:'Yup,, that works fr me ',
-
-  });
 });
 
 socket.on('disconnect',function() {
