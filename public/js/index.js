dfceacda3ce4555a85603b27c4c4a4d89dbc4e96
kavyavshaiva <kavya.k@evolvussolutions.com>
2018-02-05 15:39:07 +0530

    createMessage and newMessage events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket=io();
+socket.on('connect',function() {
+  console.log(' connected to server');
+
+  socket.emit('createMessage', {
+    from:'kavya',
+    text:'Yup,, that works fr me ',
+
+  });
+});
+
+socket.on('disconnect',function() {
+  console.log(' disconnected to server');
+});
+
+
+socket.on('newMessage',function(message) {
+  console.log(' new Message',message);
+});
--- server/server.js
+++ server/server.js
@@ -13,6 +13,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=> {
   console.log('new user connected');
 
+socket.emit('newMessage' , {
+  from:'kavyaaa',
+  text:'hey.. wht is gng on',
+  createdAt:123
+});
+
+socket.on('createMessage' ,(message)=> {
+  console.log('createMessage',message);
+});
+
 socket.on('disconnect', ()=> {
   console.log('user was disconnected');
 });
