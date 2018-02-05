02a7e4536437327f333dfb3a34a290c92cc99a61
kavyavshaiva <kavya.k@evolvussolutions.com>
2018-02-05 14:45:27 +0530

    connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const path=require('path');
 const express=require('express');
+const http=require('http');
+const socketIO=require('socket.io');
 const publicPath=path.join(__dirname,'../public');
 var app=express();
 const port=process.env.PORT || 3000;
+var server=http.createServer(app);
+var io=socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=> {
+io.on('connection', (socket)=> {
+  console.log('new user connected');
+
+socket.on('disconnect', ()=> {
+  console.log('user was disconnected');
+});
+});
+
+server.listen(port, ()=> {
   console.log(`server is up on port ${port}`);
 })
