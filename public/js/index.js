368bd36eaf20be3675c1059419f7354d56c7ea2c
kavyavshaiva <kavya.k@evolvussolutions.com>
2018-02-06 15:11:46 +0530

    Scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,23 @@
 var socket=io();
+
+function scrollToBottom() {
+ //selectors
+ var messages=jQuery('#messages');
+ var newMessage=messages.children('li:last-child');
+ //heights
+ var clientHeight=messages.prop('clientHeight');
+ var scrollTop=messages.prop('scrollTop');
+ var scrollHeight=messages.prop('scrollHeight');
+ var newMessageHeight=newMessage.innerHeight();
+ var lastMessageHeight=newMessage.prev().innerHeight();
+
+ if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=scrollHeight)
+ {
+   messages.scrollTop(scrollHeight);
+  }
+}
+
+
 socket.on('connect',function() {
   console.log(' connected to server');
 });
@@ -17,7 +36,7 @@ socket.on('newMessage',function(message) {
     createdAt:formattedTime
   });
   jQuery('#messages').append(html);
-
+scrollToBottom();
 
 //     console.log(' new Message',message);
 //
@@ -36,7 +55,7 @@ socket.on('newMessage',function(message) {
      createdAt:formattedTime
    });
    jQuery('#messages').append(html);
-
+scrollToBottom();
 
    // var li=jQuery('<li></li>');
    // var a=jQuery('<a target="_blank">MyCurrentLocation</a>');
